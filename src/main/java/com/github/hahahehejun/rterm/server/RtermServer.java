6fb56b214fa76b4ece3068162a370f9a242732ba
fengwu <781183149@qq.com>
2022-01-13 17:59:21 +0800

    init

---
+++ src/main/java/com/github/hahahehejun/rterm/server/RtermServer.java
@@ -0,0 +1,61 @@
+package com.github.hahahehejun.rterm.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
+
+/**
+ * @author wu
+ * @date 2022/1/13 16:46
+ */
+public class RtermServer {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.group(boss, worker);
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch){
+                    ch.pipeline().addLast(LOGGING_HANDLER);
+                    // 用来判断是不是 读空闲时间过长，或 写空闲时间过长
+                    // 5s 内如果没有收到 channel 的数据，会触发一个 IdleState#READER_IDLE 事件
+                    ch.pipeline().addLast(new IdleStateHandler(60, 0, 0));
+                    // ChannelDuplexHandler 可以同时作为入站和出站处理器
+                    ch.pipeline().addLast(new ChannelDuplexHandler() {
+                        // 用来触发特殊事件
+                        @Override
+                        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception{
+                            IdleStateEvent event = (IdleStateEvent) evt;
+                            // 触发了读空闲事件
+                            if (event.state() == IdleState.READER_IDLE) {
+                                ctx.channel().close();
+                            }
+                        }
+                    });
+                }
+            });
+            Channel channel = serverBootstrap.bind(8080).sync().channel();
+            channel.closeFuture().sync();
+        } catch (InterruptedException ignored) {
+
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+}
