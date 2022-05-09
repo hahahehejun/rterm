43cb320c9caf24373d06c0709492123961a56f84
wu <781183149@qq.com>
2022-05-09 13:26:51 +0800

    提交

--- src/main/java/com/github/hahahehejun/rterm/server/RtermServer.java
+++ src/main/java/com/github/hahahehejun/rterm/server/RtermServer.java
@@ -1,29 +1,27 @@
 package com.github.hahahehejun.rterm.server;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.handler.timeout.IdleStateHandler;
-import io.netty.util.CharsetUtil;
 
 /**
  * @author wu
  * @date 2022/1/13 16:46
  */
 public class RtermServer {
+    final static LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
 
     public static void main(String[] args) {
         NioEventLoopGroup boss = new NioEventLoopGroup();
         NioEventLoopGroup worker = new NioEventLoopGroup();
-        LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
         try {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap.channel(NioServerSocketChannel.class);
@@ -32,34 +30,11 @@ public class RtermServer {
                 @Override
                 protected void initChannel(SocketChannel ch){
                     ch.pipeline().addLast(LOGGING_HANDLER);
-                    // 用来判断是不是 读空闲时间过长，或 写空闲时间过长
-                    // 5s 内如果没有收到 channel 的数据，会触发一个 IdleState#READER_IDLE 事件
-                    ch.pipeline().addLast(new IdleStateHandler(60, 0, 0));
-                    // ChannelDuplexHandler 可以同时作为入站和出站处理器
-//                    ch.pipeline().addLast(new ChannelDuplexHandler() {
-//                        // 用来触发特殊事件
-//                        @Override
-//                        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception{
-//                            IdleStateEvent event = (IdleStateEvent) evt;
-//                            // 触发了读空闲事件
-//                            if (event.state() == IdleState.READER_IDLE) {
-//                                ctx.channel().close();
-//                            }
-//                        }
-
-//                    });
+                    ch.pipeline().addLast(new StringDecoder());
                     ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
                         @Override
                         public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                            ByteBuf byteBuf = (ByteBuf) msg;
-                            System.out.println(byteBuf.toString(CharsetUtil.UTF_8));
-                            ctx.writeAndFlush(Unpooled.copiedBuffer("服务端已收到消息，并给你发送一个问号?", CharsetUtil.UTF_8));
-                        }
-
-                        @Override
-                        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-                            //发送消息给客户端
-
+                            System.out.println(msg);
                         }
                     });
                 }
